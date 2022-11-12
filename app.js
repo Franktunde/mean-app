8093a8776c76861306f91a23e4c76df1039712f0
franktunde <stfrankiesjames76@gmail.com>
2022-11-12 01:28:07 +0100

    updated express route

--- app.js
+++ app.js
@@ -1,27 +1,27 @@
-const express = require("express")
-const path = require("path")
-const bodyParser = require('body-parser')
-const cors = require("cors")
-const passport = require("passport")
-const mongoose = require("mongoose")
-const config = require('./config/database')
+const express = require("express");
+const path = require("path");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const passport = require("passport");
+const mongoose = require("mongoose");
+const config = require("./config/database");
 
 //CREATE CONNECTION
-mongoose.connect(config.database)
+mongoose.connect(config.database);
 
 //CONNECTING TO DATABASE
-mongoose.connection.on("connected", ()=> {
-    console.log('Connected to database' + config.database)
-})
+mongoose.connection.on("connected", () => {
+  console.log("Connected to database" + config.database);
+});
 
 //DATABASE ERROR
-mongoose.connection.on("error", (err)=> {
-    console.log('Error connecting to database' + err)
-})
+mongoose.connection.on("error", (err) => {
+  console.log("Error connecting to database" + err);
+});
 
 const app = express();
 
-const users = require('./routes/users')
+const users = require("./routes/users");
 
 //PORT
 const port = 3000;
@@ -30,26 +30,29 @@ const port = 3000;
 app.use(cors());
 
 //Set static folder
-app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.static(path.join(__dirname, "public")));
 
 //Bodyparser middleware
-app.use(bodyParser.json())
+app.use(bodyParser.json());
 
 //passport middleware
 app.use(passport.initialize());
 app.use(passport.session());
 
-require('./config/passport')(passport);
-
+require("./config/passport")(passport);
 
 app.use("/users", users);
 
 //index route
-app.get("/", (req, res)=> {
- res.send("Invalid Endpoint")
-})
+app.get("/", (req, res) => {
+  res.send("Invalid Endpoint");
+});
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "public/index.html"));
+});
 
 //start server
-app.listen(port, ()=>{
-    console.log("server running on port: " + port)
-})
+app.listen(port, () => {
+  console.log("server running on port: " + port);
+});
